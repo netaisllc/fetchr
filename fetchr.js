230554cd4bed79297a2d0dd62e4f879296dc7e4f
Kevin McGee <kevin.mcgee@cssian.com>
2016-02-05 15:22:17 -0800

    Revise onLoad handling for slovenly server responses

--- fetchr.js
+++ fetchr.js
@@ -1,12 +1,15 @@
 /*
  *    FETCHR.js ::
- *    Simplest ever XHR utility abiding by promises, timeout
- *    and cancellation directives. Call the default export
+ *    Simplest ever client-side XHR utility abiding by promises, 
+ *    timeout and cancellation directives. Call the default export
  *    providing a URL, and optionally, a timeout limit and
  *    demand for JSON style response data.
  *
+ *    Requirements ::
+ *    A Promise implementation; we like BluebirdJS, but RSVP is nice, too.
+ *
  *    Use ::
- *    fetchr( "http://api.acme.international/fortune", 4500, true ).then(
+ *    fetchr( "http://api.acme.international/fortune", 'GET', 4500, true ).then(
  *       function ( data ) {
  *           console.log( 'Truth:', data );
  *       },
@@ -16,19 +19,20 @@
  *
  */
 
-export function fetchr( sURL, method, timeoutDuration, returnJSON ) {
+export function fetchr( sURL, method, errMessage, timeoutDuration, returnJSON ) {
     /* Return a promise object that resolves with the response value
      * or rejects with descriptive context info
      *
      * @sURL            - reqd - a URI target presumed of good format
      * @method          - opt  - HTTP verb dictating the operation, default GET
+     * @errMessage      - opt  - string to post to console when bad things happen
      * @timeoutDuration - opt  - allowed duration for the call (ms), default UNDEF
      * @returnJSON      - opt  - parse response before returning it, default TRUE
      *
      */
-
-    const identity = "Fetchr 0.2";
-
+     
+    const identity = "Fetchr 0.3";
+     
     var promiseEnabled = function() {
         // more here
         if (typeof window.Promise === 'undefined') {
@@ -39,13 +43,13 @@ export function fetchr( sURL, method, timeoutDuration, returnJSON ) {
         }
         return true;
     }
-
+    
     if ( promiseEnabled() && sURL ) {
         // OK to continue
 
         let action = method || 'GET';
         action = action.toUpperCase();
-
+        
         let timeout = timeoutDuration || undefined;
         let isJson = returnJSON || true;
 
@@ -61,7 +65,7 @@ export function fetchr( sURL, method, timeoutDuration, returnJSON ) {
             request.addEventListener( "load", function( e ) {
                 // If client specified JSON via @returnJSON,
                 // attempt to parse the response into an object
-                if ( this.response && this.responseText) {
+                if ( this.response && this.responseText && this.responseText[0] !== "<" ) {
                     var payload = isJson ? JSON.parse( this.responseText ) : this.responseText;
                     resolve( payload );
                 } else {
@@ -88,6 +92,7 @@ export function fetchr( sURL, method, timeoutDuration, returnJSON ) {
             });
 
             request.addEventListener( "error", function( e ) {
+                console.error( 'Fetchr had a bad problem:', errMessage )
                 reject( {
                     "result": action + " ERROR",
                     "status": this.status,
